4d4ec20d4642b9a2e4615ed7b3be115a04362cbc
HouariZegai <houarizegai14@gmail.com>
2020-02-22 00:37:57 +0100

    Part 10 - making get data from php using Ajax & gen HTML from XSL & XML

---
+++ App/js/main.js
@@ -0,0 +1,94 @@
+function loadXSLDoc(filename) {
+    let xhttp;
+    if (window.XMLHttpRequest) { // is modern browser?
+        xhttp = new XMLHttpRequest();
+    } else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhttp.open("GET", filename, false);
+    try {
+        xhttp.responseType = "msxml-document";
+    } catch (err) {
+    } // Helping IE11
+
+    xhttp.send("");
+
+    return xhttp.responseXML;
+}
+
+function displayResult(xhttp, response, xslFileName) {
+    let xsl = loadXSLDoc("xsl/" + xslFileName + ".xsl"),
+        xml = new DOMParser().parseFromString(response.responseText, "text/xml"),
+        htmlElement = document.getElementById("tables");
+
+    if (response == null)
+        console.log("response = null");
+
+    // code for IE
+    if (window.ActiveXObject || xhttp.responseType == "msxml-document") {
+        htmlElement.innerHTML = xml.transformNode(xsl);
+    }
+    // code for Chrome, Firefox, Opera, etc.
+    else if (document.implementation && document.implementation.createDocument) {
+        let xsltProcessor = new XSLTProcessor();
+        xsltProcessor.importStylesheet(xsl);
+
+        let resultDocument = xsltProcessor.transformToFragment(xml, document);
+
+        htmlElement.appendChild(resultDocument);
+    }
+}
+
+function getPlanning(niveau, option) {
+    let xhttp;
+
+    if (window.XMLHttpRequest) { // is modern browser?
+        xhttp = new XMLHttpRequest();
+    } else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) { // display data when response is ready and the request finished
+            displayResult(xhttp, this, "planning");
+        }
+    };
+
+    xhttp.open("GET", "php/get_planning.php?niveau=" + niveau + "&option=" + option, true);
+    xhttp.send();
+}
+
+function getPromotion(niveau, option) {
+    let xhttp;
+
+    if (window.XMLHttpRequest) { // is modern browser?
+        xhttp = new XMLHttpRequest();
+    } else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) { // display data when response is ready and the request finished
+            displayResult(xhttp, this, "promotion");
+        }
+    };
+
+    xhttp.open("GET", "php/get_promotion.php?niveau=" + niveau + "&option=" + option, true);
+    xhttp.send();
+}
+
+function getPlanningAndPromotion(niveau, option) {
+    document.getElementById("promo").innerHTML = niveau + option;
+
+    /* remove all children */
+    var tables = document.getElementById("tables");
+    var child = tables.lastElementChild;
+    while (child) {
+        tables.removeChild(child);
+        child = tables.lastElementChild;
+    }
+
+    getPlanning(niveau, option);
+    getPromotion(niveau, option);
+}
