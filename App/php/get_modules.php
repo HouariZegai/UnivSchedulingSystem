431693f50698cfa3ddfbc1f93312265acb663fcb
HouariZegai <houarizegai14@gmail.com>
2020-02-22 18:36:06 +0100

    get data of add planning form (profs, modules & salles) from database

---
+++ App/php/get_modules.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'db_config.php';
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT nom_mod FROM modules;";
+
+$result = $conn->query($sql);
+
+if(!$result)
+      echo 'sql failure:', $conn->error, ': ', $sql;
+
+if ($result->num_rows > 0) {
+
+    $out = array();
+
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $out[] = $row["nom_mod"];
+    }
+
+    echo json_encode($out);
+
+} else {
+    echo "0 results";
+}
+
+$conn->close();
